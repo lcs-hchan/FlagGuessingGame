feb40e2ce1aff6dc922cf5cd3627cb712492fe84
Hayden Chan <hchan@lcs.on.ca>
2025-04-03 18:54:34 -0400

    Initial Commit

---
+++ FlagGuessingGame/FlagGuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessingGameApp.swift
+//  FlagGuessingGame
+//
+//  Created by hayden on 2025-04-03.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
